4d17a380d157b8164aa0eb24f80e2c57f0adf7f4
Chenshiwen <cswzmg@live.com>
2017-07-28 17:29:22 +0800

    滤镜使用

--- SceneKit-19滤镜/ViewController.m
+++ SceneKit-19滤镜/ViewController.m
@@ -7,19 +7,104 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController ()
 
+@property (nonatomic,strong)SCNView *mScnView;
+@property (weak, nonatomic) IBOutlet UIView *preview;
+@property (nonatomic,strong) SCNNode *boxNode;
+@property (nonatomic,strong) CIFilter* preViewOne,*preViewTwo,*preViewThree;
+@property (nonatomic,strong) NSMutableArray *muArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.mScnView];
+    self.muArray = [[NSMutableArray alloc] init];
+    
+    SCNNode *cameraNdoe = [SCNNode node];
+    cameraNdoe.camera = [SCNCamera camera];
+    cameraNdoe.position = SCNVector3Make(0, 0, 30);
+    [self.mScnView.scene.rootNode addChildNode:cameraNdoe];
+    
+    self.boxNode = [SCNNode nodeWithGeometry:[SCNBox boxWithWidth:10 height:10 length:10 chamferRadius:0]];
+    self.boxNode.geometry.firstMaterial.diffuse.contents = @"box2.jpg";
+    [self.mScnView.scene.rootNode addChildNode:self.boxNode];
+    
+}
+#warning 更多效果，详见官方文档
+//效果一
+-(CIFilter *)preViewOne{
+    if (!_preViewOne) {
+        _preViewOne = [CIFilter filterWithName:@"CIComicEffect"];
+    }
+    return _preViewOne;
 }
 
+//效果二
+-(CIFilter *)preViewTwo{
+    if (!_preViewTwo) {
+        _preViewTwo = [CIFilter filterWithName:@"CICrystallize"];
+    }
+    return _preViewTwo;
+}
+//效果三
+-(CIFilter *)preViewThree{
+    if (!_preViewThree) {
+        _preViewThree = [CIFilter filterWithName:@"CICircularScreen"];
+    }
+    return _preViewThree;
+}
 
+- (IBAction)selectAction:(UISwitch *)sender {
+    NSInteger tag = sender.tag;
+    BOOL isOn = sender.isOn;
+    
+    switch (tag) {
+        case 0:
+            NSLog(@"效果一");
+            if (isOn) {
+                [self.muArray addObject:self.preViewOne];
+            }else{
+                [self.muArray removeObject:self.preViewOne];
+            }
+            break;
+        case 1:
+            if (isOn) {
+                [self.muArray addObject:self.preViewTwo];
+            }else{
+                [self.muArray removeObject:self.preViewTwo];
+            }
+            NSLog(@"效果二");
+            break;
+        case 2:
+            if (isOn) {
+                [self.muArray addObject:self.preViewThree];
+            }else{
+                [self.muArray removeObject:self.preViewThree];
+            }
+            NSLog(@"效果三");
+            break;
+            
+        default:
+            break;
+    }
+    self.boxNode.filters = self.muArray;
+}
+
+-(SCNView *)mScnView{
+    if (!_mScnView) {
+        _mScnView = [[SCNView alloc] initWithFrame:self.view.bounds];
+        _mScnView.allowsCameraControl = YES;
+        _mScnView.scene = [SCNScene scene];
+        _mScnView.backgroundColor = [UIColor blackColor];
+        [_mScnView addSubview:self.preview];
+    }
+    return _mScnView;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
